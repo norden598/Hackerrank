9f229bd8ad248af9360b5415f7ada423db52197a
Austin <norden598@gmail.com>
2021-02-28 22:23:10 -0500

    added day 1

---
+++ 30_Days_of_Code/Day_1_Data_Types.cs
@@ -0,0 +1,13 @@
+// Declare second integer, double, and String variables.
+        int ii = Convert.ToInt32(Console.ReadLine());
+        double dd = Convert.ToDouble(Console.ReadLine());
+        string ss = Console.ReadLine();
+        // Read and save an integer, double, and String to your variables.
+        
+        // Print the sum of both integer variables on a new line.
+        Console.WriteLine(i + ii);
+        // Print the sum of the double variables on a new line.
+        Console.WriteLine("{0:0.0}", d + dd);
+        // Concatenate and print the String variables on a new line
+        Console.WriteLine(s + ss);
+        // The 's' variable above should be printed first.
